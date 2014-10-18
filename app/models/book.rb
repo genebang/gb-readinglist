cb22545a6c44f649de7042e13a542a2ec02fa782
Gene Bang <genebang@gmail.com>
2014-10-17 18:48:29 -0700

    Adds better search.

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,10 @@ class Book < ActiveRecord::Base
   # scope :finished, ->{ where('finished_on IS NOT NULL') }
   scope :finished, ->{ where.not(finished_on: nil) }
   scope :recent,   ->{ where('finished_on > ?', 2.days.ago) }
-  scope :search,   ->(keyword) { where(title: keyword) if keyword.present? }
+  # scope :search,   ->(keyword) { where(title: keyword) if keyword.present? }
+  scope :search,   ->(keyword) { where('keywords LIKE ?', "%#{keyword.downcase}%") if keyword.present? }
+
+  before_save :set_keywords
 
   # def self.recent
   #   where('finished_on > ?', 2.days.ago)
@@ -20,5 +23,12 @@ class Book < ActiveRecord::Base
   #     all
   #   end
   # end
+
+  protected
+
+    def set_keywords
+      # self.keywords = [title, author, description].map { |p| p.downcase }
+      self.keywords = [title, author, description].map(&:downcase).join(' ')
+    end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141017230736) do
+ActiveRecord::Schema.define(version: 20141018013039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20141017230736) do
     t.datetime "updated_at"
     t.integer  "rating"
     t.date     "finished_on"
+    t.text     "keywords"
   end
 
 end
