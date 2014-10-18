417b123b55b319c676a702428bf0077793423bbb
Gene Bang <genebang@gmail.com>
2014-10-17 17:37:41 -0700

    Initial commit. Book list with the basics.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReadingList::Application.config.secret_key_base = '3fbf186e8966970fbfb11283e9ab48d79ef352ab11fd3a84819d0a196e53dc6b7dc673539981725ebe095c4aa4e3d047d641af812ac3efc975822b2be5ea8a38'
